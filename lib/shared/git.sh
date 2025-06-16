a579f9d69455576912c8a166a72bc4386f9c3107
underluns <underluns@gmail.com>
2025-06-16 15:44:47 +0400

    Rename functions and variables

--- lib/shared/git.sh
+++ lib/shared/git.sh
@@ -20,14 +20,18 @@ __terminalis_git_revision() {
   git rev-parse --short HEAD 2> /dev/null
 }
 
-__terminalis_git_count_commits_diff() {
+__terminalis_git_ref() {
+  __terminalis_git_branch || __terminalis_git_tag || __terminalis_git_revision
+}
+
+__terminalis_git_count_diff() {
   git rev-list --count "$1".."$2" 2> /dev/null
 }
 
-__terminalis_git_count_commits_ahead() {
-  __terminalis_git_count_commits_diff "origin/$1" "$1"
+__terminalis_git_count_ahead() {
+  __terminalis_git_count_diff "origin/$1" "$1"
 }
 
-__terminalis_git_count_commits_behind() {
-  __terminalis_git_count_commits_diff "$1" "origin/$1"
+__terminalis_git_count_behind() {
+  __terminalis_git_count_diff "$1" "origin/$1"
 }
