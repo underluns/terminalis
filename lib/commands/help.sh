4b245fd9eb78fdf2f98d1c75f45ea13c6860a315
underluns <underluns@gmail.com>
2025-09-19 16:01:24 +0400

    Load paths to terminalis with using a separate function

--- lib/commands/help.sh
+++ lib/commands/help.sh
@@ -3,19 +3,19 @@
 terminalis_help() {
   case "$1" in
     '')
-      cat "$(__terminalis_root)/help/terminalis.txt"
+      __terminalis_cat "help/terminalis.txt"
       terminalis_help init
       terminalis_help update
       terminalis_help help
       ;;
     'init')
-      cat "$(__terminalis_root)/help/commands/init.txt"
+      __terminalis_cat "help/commands/init.txt"
       ;;
     'update')
-      cat "$(__terminalis_root)/help/commands/update.txt"
+      __terminalis_cat "help/commands/update.txt"
       ;;
     'help' | '--help' | '-h')
-      cat "$(__terminalis_root)/help/commands/help.txt"
+      __terminalis_cat "help/commands/help.txt"
       ;;
     *)
       __terminalis_error_command_not_found "$1"
