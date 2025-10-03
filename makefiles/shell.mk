1312f80a784d8e24c576320f741b3869777dfefb
underluns <underluns@gmail.com>
2025-10-03 10:32:19 +0400

    Fix shell checks

--- makefiles/shell.mk
+++ makefiles/shell.mk
@@ -3,11 +3,11 @@ shell_install:
 	asdf plugin add shfmt && asdf install shfmt
 	asdf plugin add shellcheck && asdf install shellcheck
 
-.PHONY: shell_lint
-shell_lint:
-	shfmt --version && shfmt --diff .
-	shellcheck --version && shellcheck $(shell find . -name '*.sh')
-
 .PHONY: shell_format
 shell_format:
 	shfmt --version && shfmt --list --write .
+
+.PHONY: shell_lint
+shell_lint:
+	shfmt --version && shfmt --diff .
+	shellcheck --version && shellcheck $(shell find . -type f -name '*.sh')
