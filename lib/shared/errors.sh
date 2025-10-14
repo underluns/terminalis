f9e6d79e8442bb1c7884a49688e2f2dfc2f05bc0
underluns <underluns@gmail.com>
2025-10-14 11:52:49 +0400

    Return error code

--- lib/shared/errors.sh
+++ lib/shared/errors.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
 __terminalis_error() {
-  echo "terminalis: $1" 1>&2 && return 1
+  echo "terminalis: ${1:-"error"}" 1>&2 && return "${2:-1}"
+}
+
+__terminalis_error_status() {
+  set -- "${1:-"$?"}"
+
+  echo "$1" 1>&2 && return "$1"
 }
 
 __terminalis_error_command_not_found() {
-  __terminalis_error "command not found: $1"
+  __terminalis_error "command not found: ${1:?}" 127
 }
