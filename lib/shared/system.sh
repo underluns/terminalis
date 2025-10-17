f69631db868b3f15c59929f413337d48a112ca19
underluns <underluns@gmail.com>
2025-10-17 16:41:59 +0400

    Fix snap update

--- lib/shared/system.sh
+++ lib/shared/system.sh
@@ -4,26 +4,20 @@ __terminalis_system_software_is_exists() {
   __terminalis_shell_command_is_exists softwareupdate
 }
 
-__terminalis_system_brew_is_exists() {
-  __terminalis_shell_command_is_exists brew
-}
-
 __terminalis_system_apt_is_exists() {
   __terminalis_shell_command_is_exists apt
 }
 
+__terminalis_system_brew_is_exists() {
+  __terminalis_shell_command_is_exists brew
+}
+
+__terminalis_system_snap_is_exists() {
+  __terminalis_shell_command_is_exists snap
+}
+
 __terminalis_system_software_update() {
   softwareupdate --install --all --agree-to-license
-}
-
-__terminalis_system_brew_update() {
-  brew update --force &&
-    brew upgrade &&
-    brew upgrade --cask --greedy
-}
-
-__terminalis_system_brew_clean() {
-  brew cleanup --prune=all
 }
 
 __terminalis_system_apt_update() {
@@ -36,4 +30,18 @@ __terminalis_system_apt_clean() {
   sudo apt autoremove --purge --yes &&
     sudo apt autoclean --yes &&
     sudo apt clean --yes
+}
+
+__terminalis_system_brew_update() {
+  brew update --force &&
+    brew upgrade &&
+    brew upgrade --cask --greedy
+}
+
+__terminalis_system_brew_clean() {
+  brew cleanup --prune=all
+}
+
+__terminalis_system_snap_update() {
+  sudo snap refresh
 }
