a8c7ecf099edb90df8b687ebca6778a470e8688b
underluns <underluns@gmail.com>
2025-10-17 14:55:25 +0400

    Fix format

--- lib/shared/system.sh
+++ lib/shared/system.sh
@@ -17,9 +17,9 @@ __terminalis_system_software_update() {
 }
 
 __terminalis_system_brew_update() {
-  brew update --force && \
-  brew upgrade && \
-  brew upgrade --cask --greedy
+  brew update --force &&
+    brew upgrade &&
+    brew upgrade --cask --greedy
 }
 
 __terminalis_system_brew_clean() {
@@ -27,13 +27,13 @@ __terminalis_system_brew_clean() {
 }
 
 __terminalis_system_apt_update() {
-  sudo apt update --list-cleanup && \
-  sudo apt upgrade --assume-yes --auto-remove && \
-  sudo apt full-upgrade --assume-yes --auto-remove 
+  sudo apt update --list-cleanup --yes &&
+    sudo apt upgrade --yes &&
+    sudo apt full-upgrade --yes
 }
 
 __terminalis_system_apt_clean() {
-  sudo apt autoremove --assume-yes --purge && \
-  sudo apt autoclean --assume-yes && \
-  sudo apt clean --assume-yes
+  sudo apt autoremove --purge --yes &&
+    sudo apt autoclean --yes &&
+    sudo apt clean --yes
 }
